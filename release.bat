06249bc1274e234f5cc0b5024d64151877dabe07
Karl Gong <karl.gong@outlook.com>
2019-03-03 17:50:11 +0800

    Finished.

--- release.bat
+++ release.bat
@@ -1,16 +1,17 @@
 rd dist /s /q
 
 cd CrossFileApi
+dotnet clean -c release -r win7-x64
 dotnet publish -c release -r win7-x64
 cd ..
-xcopy CrossFileApi\bin\Release\netcoreapp2.2\win7-x64 dist\ /s /e /c /y /h /r
+xcopy CrossFileApi\bin\Release\netcoreapp2.2\win7-x64\publish dist\ /s /e /c /y /h /r
 
 cd CrossFileWap
-npm run release
+cmd /C npm run release
 cd ..
-xcopy CrossFileWap\dist dist\publish\wwwroot\wap /e
+xcopy CrossFileWap\dist dist\wwwroot\wap /e
 
 cd CrossFileWeb
-npm run release
+cmd /C npm run release
 cd ..
-xcopy CrossFileWeb\dist dist\publish\wwwroot\web /e
+xcopy CrossFileWeb\dist dist\wwwroot\web /e
